dddadbfdf33d2d88c03abb396dd8d99115fc4a4c
MidhunViknCodes <midhunvikncodes@gmail.com>
2026-01-09 18:00:25 +0530

    Refactor code structure for improved readability and maintainability

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "196953180"
+    serviceWorkerVersion: "4282045030"
   }
 });
